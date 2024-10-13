d19821fa1dcb0b76e9dad4b8d06541b18cccebbb
swsuper3 <144061270+swsuper3@users.noreply.github.com>
2024-10-13 11:05:28 +0200

    Added Program/Command structure

---
+++ Command.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLearningApp
+{
+    public abstract class Command
+    {
+    }
+
+    public class MoveCommand : Command
+    {
+        int amountToMove;
+
+        public MoveCommand(int amountToMove)
+        {
+            this.amountToMove = amountToMove;
+        }
+    }
+
+    public class TurnCommand : Command
+    {
+        LeftRight turnDirection;
+
+        public TurnCommand(LeftRight turnDirection)
+        {
+            this.turnDirection = turnDirection;
+        }
+    }
+
+    public class RepeatCommand : Command
+    {
+        Program programToRepeat;
+        int amountOfRepeats;
+
+        public RepeatCommand(Program program, int repeats)
+        {
+            programToRepeat = program;
+            amountOfRepeats = repeats;
+        }
+    }
+
+    public class Program
+    {
+        List<Command> commands;
+
+        public void AddCommand(Command c)
+        {
+            commands.Add(c);
+        }
+    }
+
+    public enum LeftRight {
+        Left,
+        Right
+    }
+
+}
