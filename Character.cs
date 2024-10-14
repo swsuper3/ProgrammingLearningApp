713ac5c232bb6c3db422e8e2ffc2e279134f43fe
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-14 14:33:30 +0200

    Added Move and Turn methods to the Character.

--- Character.cs
+++ Character.cs
@@ -25,8 +25,56 @@ namespace ProgrammingLearningApp
 
             this.position = new Point();
         }
+
+        /// <summary>
+        /// This method moves the character a specific amount in the direction the character is currently facing.
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Move(int amount)
+        {
+            switch (viewDirection.Value)
+            {
+                case Direction.North:
+                    position.y -= amount;
+                    break;
+                case Direction.East:
+                    position.x += amount;
+                    break;
+                case Direction.South:
+                    position.y += amount;
+                    break;
+                case Direction.West:
+                    position.x -= amount;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// This method turns the character to the left or right (changing the viewDirection), depending on the parameter.
+        /// </summary>
+        /// <param name="turnDirection"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void Turn(LeftRight turnDirection)
+        {
+            if (turnDirection == LeftRight.Left)
+            {
+                viewDirection = viewDirection.PreviousOrLast();
+            }
+            else if (turnDirection == LeftRight.Right)
+            {
+                viewDirection = viewDirection.NextOrFirst();
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Invalid turnDirection");
+            }
+        }
     }
 
+    /// <summary>
+    /// These methods contain circular logic, so that turning the character is circular as well.
+    /// If there is no next node, it takes the first. If there is no previous node, it takes the last.
+    /// </summary>
     static class CircularLinkedList //https://stackoverflow.com/questions/716256/creating-a-circularly-linked-list-in-c
     {
         public static LinkedListNode<T> NextOrFirst<T>(this LinkedListNode<T> current)
