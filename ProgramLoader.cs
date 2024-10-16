ee10f99ab0321787d622ed21262f9296ab2146a1
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-16 21:53:19 +0200

    Added program loader class

---
+++ ProgramLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLearningApp
+{
+    public class ProgramLoader
+    {
+        public Program CreateProgram(string filename)
+        {
+            return new Program(ReadFile(filename));
+        }
+
+        public List<string> ReadFile(string filename)
+        {
+            List<string> lines = new List<string>();
+            StreamReader sr = new StreamReader(filename);
+            string line = sr.ReadLine();
+
+            while (line != null)
+            {
+                lines.Add(line);
+                line = sr.ReadLine();
+            }
+
+            return lines;
+        }
+    }
+}
