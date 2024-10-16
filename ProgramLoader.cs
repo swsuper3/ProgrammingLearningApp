9f76f57a86cbffbb2d664b67236046f41088e297
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-16 22:11:11 +0200

    Added comments to ProgramLoader

--- ProgramLoader.cs
+++ ProgramLoader.cs
@@ -8,11 +8,21 @@ namespace ProgrammingLearningApp
 {
     public class ProgramLoader
     {
+        /// <summary>
+        /// This method creates and returns a program from the specified filename.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
         public Program CreateProgram(string filename)
         {
             return new Program(ReadFile(filename));
         }
 
+        /// <summary>
+        /// This methods simply reads a file and puts each line into a list.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
         public List<string> ReadFile(string filename)
         {
             List<string> lines = new List<string>();
