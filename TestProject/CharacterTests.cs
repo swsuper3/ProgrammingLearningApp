d6d6be7f86db6e6fd5ccd6dfdce7cff6ef9b5ff6
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-16 13:51:53 +0200

    Added unit test for Character Move method

--- TestProject/CharacterTests.cs
+++ TestProject/CharacterTests.cs
@@ -17,5 +17,21 @@ namespace TestProject
             //Assert
             Assert.Equal(Direction.West, c.ViewDirection);
         }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(0)]
+        [InlineData(99999)]
+        public void Move_Character_East(int amountToMove)
+        {
+            //Arrange
+            Character c = new Character();
+
+            //Act
+            c.Move(amountToMove);
+
+            //Assert
+            Assert.Equal(amountToMove, c.Position.x);
+        }
     }
 }
