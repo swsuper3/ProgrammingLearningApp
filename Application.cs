59595b4764c432780633bd3ceeb21a470e16bc7b
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-17 11:13:50 +0200

    Added application that runs a program and outputs end state.

---
+++ Application.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLearningApp
+{
+    public class Application
+    {
+        ProgramLoader programLoader = new ProgramLoader();
+
+        public Application()
+        {
+            programLoader = new ProgramLoader();
+        }
+
+        public void RunProgram(string filename)
+        {
+            Character character = new Character();
+            Program program = programLoader.CreateProgram(filename);
+
+            program.Execute(character);
+
+            Console.WriteLine("End state "+character.Position+" facing "+character.ViewDirection);
+        }
+    }
+}
