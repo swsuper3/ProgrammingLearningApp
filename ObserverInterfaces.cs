28ec0f214002a812bff230c1006a6a6672899f22
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-22 16:28:03 +0200

    Added some interfaces for the observer pattern.

---
+++ ObserverInterfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLearningApp
+{
+    public interface ISubject<T>
+    {
+        public void Attach(IMyObserver<T> observer);
+        public void Detach(IMyObserver<T> observer);
+        public void Notify();
+    }
+
+    public interface IMyObserver<T>
+    {
+        public void Update(T subject);
+    }
+}
