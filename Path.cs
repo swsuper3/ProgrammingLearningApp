68e66cf284cce9993324d2572fa7a68a40a5f7ae
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-10-23 15:12:26 +0200

    Added path class

---
+++ Path.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLearningApp
+{
+    public class Path : IMyObserver<Character>
+    {
+        List<Point> cellsAlongPath;
+
+        public Path()
+        {
+            cellsAlongPath = new List<Point>();
+        }
+
+        public void Update(Character character)
+        {
+            cellsAlongPath.Add(character.Position);
+        }
+
+    }
+}
