adb5f66859a78185f1202cc911a125680700d2b8
woekkk <wieke.yeehaw@gmail.com>
2024-10-24 11:32:06 +0200

    Added start of basic design

--- WindowsForm.cs
+++ WindowsForm.cs
@@ -10,36 +10,103 @@ using System.Windows;
 
 namespace ProgrammingLearningApp
 {
-    // For using the Form, we used the following guide:
+    // For using the Form, we used the following guides:
     // https://learn.microsoft.com/en-us/dotnet/desktop/winforms/how-to-create-a-windows-forms-application-from-the-command-line?view=netframeworkdesktop-4.8
-    
+    // https://learn.microsoft.com/en-us/visualstudio/ide/create-csharp-winform-visual-studio?view=vs-2022
+
     public class WindowsForm : Form
     {
-        public Button runButton;
 
         public WindowsForm()
         {
-            runButton = new Button()
-            {
-                Size = new System.Drawing.Size(40, 40),
-                Location = new System.Drawing.Point(30, 30),
-                Text = "Run"
-            };
-            this.Controls.Add(runButton);
-            runButton.Click += new EventHandler(runButton_Click);
-        }
-
-        private void runButton_Click(object sender, EventArgs e)
-        {
-            System.Windows.Forms.MessageBox.Show("Running...");
         }
 
         [STAThread]         // This specifies that our app is a single-threaded apartment
         public static void Main()
         {
-            System.Windows.Forms.Application.EnableVisualStyles();
+            Initialize();
             System.Windows.Forms.Application.Run();
         }
-        
+
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(170, 528);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(150, 46);
+            button1.TabIndex = 0;
+            button1.Text = "Run";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Basic", "Advanced", "Expert", "From file..." });
+            comboBox1.Location = new System.Drawing.Point(12, 27);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new System.Drawing.Size(242, 40);
+            comboBox1.TabIndex = 2;
+            comboBox1.Text = "Load program";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(343, 528);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(150, 46);
+            button2.TabIndex = 3;
+            button2.Text = "Metrics";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // WindowsForm
+            // 
+            ClientSize = new System.Drawing.Size(921, 770);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Name = "WindowsForm";
+            Load += WindowsForm_Load;
+            ResumeLayout(false);
+        }
+
+        public static void Initialize()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private TextBox textBox1;
+        private MenuStrip menuStrip1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Button button1;
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WindowsForm_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
