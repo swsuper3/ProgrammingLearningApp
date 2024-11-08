ec086d3f69b99a757da567301466f3905f03ffd7
Siem van Opstal <siem.van.opstal@xs4all.nl>
2024-11-08 15:23:09 +0100

    Added mock test for pathfinding

---
+++ MockExercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLearningApp
+{
+    public class MockExercise : PathfindingExercise
+    {
+        public MockExercise(string input) : base(input)
+        {
+
+        }
+
+        public override List<string> ReadFile(string fileName)
+        {
+            return fileName.Split(';').ToList();
+        }
+    }
+}
